72557e8d60bbcdd4687627c366150246a35fe312
Melvin Adu <contactmelvinadu@gmail.com>
2021-12-02 23:32:46 +0000

    refactor function in seperate module and created M&C tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,31 @@
-const request = require('request');
-const args = process.argv.slice(2);
+const request = require('request')
 
-let breed = args[0];
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-
+const fetchBreedDescription = function(breedName, callback) {
   
-  const data = JSON.parse(body);
-  console.log(data[0].description)
-});
+  
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback(null);
+      return
+    }
+    if (error) {
+      console.log("API Error", error.message);
+      return
+    }
+
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+  
+    callback(null, data[0].description);  
+    
+
+  });
+
+};
+
+module.exports = { fetchBreedDescription } ;
